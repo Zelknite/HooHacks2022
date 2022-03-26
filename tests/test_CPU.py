2df2b799e25b6f514d23e282f9bc66764ce33ce0
Anubhav Acharya <anubhavacharya@icloud.com>
2022-03-26 16:05:48 -0400

    Set tests up

--- tests/test_CPU.py
+++ tests/test_CPU.py
@@ -1,8 +1,14 @@
 import pytest
 
 def bit1error():
-    raise SystemExit()
+	raise SystemExit()
 
-def test_mytest():
-    with pytest.raises(SystemExit):
-        bit1error()
+def bit2error():
+	return 2
+
+def test_bit1():
+	with pytest.raises(SystemExit):
+		bit1error()
+
+def test_bit2():
+	assert bit2error() == 1
