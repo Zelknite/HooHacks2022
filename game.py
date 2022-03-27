71ba0eaa599485e26c711010990e87c5240aeb3f
Anubhav Acharya <anubhavacharya@icloud.com>
2022-03-27 01:38:28 -0400

    Completed hackathon

--- game.py
+++ game.py
@@ -5,6 +5,11 @@ pygame.init()
 Clock = pygame.time.Clock()
 MAX_FRAME_RATE = 120
 
+size = width, height = 1920, 1080
+screen = pygame.display.set_mode(size)
+speed = [2, 2]
+background_color = 255, 255, 255
+
 # The animating heart
 class Heart(pygame.sprite.Sprite):
 	def __init__(self, pos_x = 0, pos_y = 0):
@@ -21,7 +26,7 @@ class Heart(pygame.sprite.Sprite):
 		self.dragging = False
 
 	def update(self):
-		self.current_frame += 0.1
+		self.current_frame += 0.2
 		if self.current_frame >= len(self.animation_frames):
 			self.current_frame = 0
 			self.is_animating = False
@@ -29,59 +34,75 @@ class Heart(pygame.sprite.Sprite):
 		self.rect = self.image.get_rect()
 		self.rect.center = [self.pos_x, self.pos_y]
 
+# class CPU(pygame.sprite.Sprite):
+# 	def __init__(self, pos_x = 0, pos_y = 0):
+# 		super().__init__()
+# 		# Image and animation
+# 		self.images = [pygame.image.load(f"Images/CPU.png")]
+# 		self.image = self.images[0]
+# 		self.current_frame = 0
+# 		self.rect = self.image.get_rect()
+# 		self.pos_x = pos_x
+# 		self.pos_y = pos_y
+# 		self.rect.topleft = [self.pos_x, self.pos_y]
+# 		self.dragging = False
 
-class CPU(pygame.sprite.Sprite):
-	def __init__(self, pos_x = 0, pos_y = 0):
-		super().__init__()
-		# Image and animation
-		self.images = [pygame.image.load(f"Images/CPU.png")]
-		self.image = self.images[0]
-		self.current_frame = 0
-		self.rect = self.image.get_rect()
-		self.pos_x = pos_x
-		self.pos_y = pos_y
-		self.rect.topleft = [self.pos_x, self.pos_y]
+# 	def update(self):
+# 		pass
 
-class Memory(pygame.sprite.Sprite):
-	def __init__(self, pos_x = 0, pos_y = 0):
-		super().__init__()
-		# Image and animation
-		self.images = [pygame.image.load(f"Images/Memory.png")]
-		self.image = self.images[0]
-		self.current_frame = 0
-		self.rect = self.image.get_rect()
-		self.pos_x = pos_x
-		self.pos_y = pos_y
-		self.rect.topleft = [self.pos_x, self.pos_y]
+# class Memory(pygame.sprite.Sprite):
+# 	def __init__(self, pos_x = 0, pos_y = 0):
+# 		super().__init__()
+# 		# Image and animation
+# 		self.images = [pygame.image.load(f"Images/Memory.png")]
+# 		self.image = self.images[0]
+# 		self.current_frame = 0
+# 		self.rect = self.image.get_rect()
+# 		self.pos_x = pos_x
+# 		self.pos_y = pos_y
+# 		self.rect.topleft = [self.pos_x, self.pos_y]
+# 		self.dragging = False
 
-class Power(pygame.sprite.Sprite):
-	def __init__(self, pos_x = 0, pos_y = 0):
-		super().__init__()
-		# Image and animation
-		self.images = [pygame.image.load(f"Images/BetterPower.gif")]
-		self.image = self.images[0]
-		self.current_frame = 0
-		self.rect = self.image.get_rect()
-		self.pos_x = pos_x
-		self.pos_y = pos_y
-		self.rect.topleft = [self.pos_x, self.pos_y]
+# 	def update(self):
+# 		pass
 
+# class Power(pygame.sprite.Sprite):
+# 	def __init__(self, pos_x = 0, pos_y = 0):
+# 		super().__init__()
+# 		# Image and animation
+# 		self.images = [pygame.image.load(f"Images/power.png")]
+# 		self.image = self.images[0]
+# 		self.current_frame = 0
+# 		self.rect = self.image.get_rect()
+# 		self.pos_x = pos_x
+# 		self.pos_y = pos_y
+# 		self.rect.topleft = [self.pos_x, self.pos_y]
+# 		self.dragging = False
 
-
-size = width, height = 1280, 720
-screen = pygame.display.set_mode(size)
-speed = [2, 2]
-background_color = 255, 255, 255
+# 	def update(self):
+# 		pass
 
 animated_sprites = pygame.sprite.Group()
 heart = Heart(10, 10)
-power = Power(10, 10)
-memory = Memory(10, 10)
-cpu = CPU(10,10)
 animated_sprites.add(heart)
-animated_sprites.add(power)
-animated_sprites.add(memory)
-animated_sprites.add(cpu)
+
+# Non-animating objects
+power = pygame.image.load("Images/power.png")
+power_pos = [10, 10]
+power_rect = power.get_rect()
+power_dragging = False
+memory = pygame.image.load("Images/Memory.png")
+memory_pos = [100, 100]
+memory_rect = memory.get_rect()
+memory_dragging = False
+cpu = pygame.image.load("Images/CPU.png")
+cpu_pos = [200, 200]
+cpu_rect = cpu.get_rect()
+cpu_dragging = False
+clock_without_heart = pygame.image.load("Images/Clock_without_heart.png")
+clock_without_heart_pos = [300, 300]
+clock_without_heart_rect = clock_without_heart.get_rect()
+clock_without_heart_dragging = False
 
 while 1:
 	# Handles the close button
@@ -92,22 +113,69 @@ while 1:
 		if event.type == pygame.MOUSEBUTTONDOWN:
 			if heart.rect.collidepoint(event.pos):
 				heart.dragging = True
-				mouse_x, mouse_y = event.pos
-				offset_x = heart.pos_x - mouse_x
-				offset_y = heart.pos_y - mouse_y
+				heart_mouse_x, heart_mouse_y = event.pos
+				heart_offset_x = heart.pos_x - heart_mouse_x
+				heart_offset_y = heart.pos_y - heart_mouse_y
+			elif power_rect.collidepoint(event.pos):
+				power_dragging = True
+				power_mouse_x, power_mouse_y = event.pos
+				power_offset_x = power_pos[0] - power_mouse_x
+				power_offset_y = power_pos[1] - power_mouse_y
+			elif memory_rect.collidepoint(event.pos):
+				memory_dragging = True
+				memory_mouse_x, memory_mouse_y = event.pos
+				memory_offset_x = memory_pos[0] - memory_mouse_x
+				memory_offset_y = memory_pos[1] - memory_mouse_y
+			elif cpu_rect.collidepoint(event.pos):
+				cpu_dragging = True
+				cpu_mouse_x, cpu_mouse_y = event.pos
+				cpu_offset_x = cpu_pos[0] - cpu_mouse_x
+				cpu_offset_y = cpu_pos[1] - cpu_mouse_y
+			elif clock_without_heart_rect.collidepoint(event.pos):
+				clock_without_heart_dragging = True
+				clock_without_heart_mouse_x, clock_without_heart_mouse_y = event.pos
+				clock_without_heart_offset_x = clock_without_heart_pos[0] - clock_without_heart_mouse_x
+				clock_without_heart_offset_y = clock_without_heart_pos[1] - clock_without_heart_mouse_y
 		elif event.type == pygame.MOUSEBUTTONUP:
 			if event.button == 1:
 				heart.dragging = False
+				power_dragging = False
+				memory_dragging = False
+				cpu_dragging = False
+				clock_without_heart_dragging = False
 		elif event.type == pygame.MOUSEMOTION:
 			if heart.dragging:
-				mouse_x, mouse_y = event.pos
-				if ((mouse_x + offset_x + heart.image.get_width()) > 0 and (mouse_x + offset_x + heart.image.get_width()) < width):
-					heart.pos_x = mouse_x + offset_x
-				if ((mouse_y + offset_y + heart.image.get_height()) > 0 and (mouse_y + offset_y + heart.image.get_height()) < height):
-					heart.pos_y = mouse_y + offset_y
+				heart_mouse_x, heart_mouse_y = event.pos
+				if ((heart_mouse_x + heart_offset_x + heart.image.get_width()) > 0 and (heart_mouse_x + heart_offset_x + heart.image.get_width()) <= width):
+					heart.pos_x = heart_mouse_x + heart_offset_x
+				if ((heart_mouse_y + heart_offset_y + heart.image.get_height()) > 0 and (heart_mouse_y + heart_offset_y + heart.image.get_height()) <= height):
+					heart.pos_y = heart_mouse_y + heart_offset_y
+			elif power_dragging:
+				power_mouse_x, power_mouse_y = event.pos
+				if ((power_mouse_x + power_offset_x + power.get_width()) > 0 and (power_mouse_x + power_offset_x + power.get_width()) <= width):
+					power_pos[0] = power_mouse_x + power_offset_x
+				if ((power_mouse_y + power_offset_y + power.get_height()) > 0 and (power_mouse_y + power_offset_y + power.get_height()) <= height):
+					power_pos[1] = power_mouse_y + power_offset_y
+			elif memory_dragging:
+				memory_mouse_x, memory_mouse_y = event.pos
+				if ((memory_mouse_x + memory_offset_x + memory.get_width()) > 0 and (memory_mouse_x + memory_offset_x + memory.get_width()) <= width):
+					memory_pos[0] = memory_mouse_x + memory_offset_x
+				if ((memory_mouse_y + memory_offset_y + memory.get_height()) > 0 and (memory_mouse_y + memory_offset_y + memory.get_height()) <= height):
+					memory_pos[1] = memory_mouse_y + memory_offset_y
+			elif cpu_dragging:
+				cpu_mouse_x, cpu_mouse_y = event.pos
+				if ((cpu_mouse_x + cpu_offset_x + cpu.get_width()) > 0 and (cpu_mouse_x + cpu_offset_x + cpu.get_width()) <= width):
+					cpu_pos[0] = cpu_mouse_x + cpu_offset_x
+				if ((cpu_mouse_y + cpu_offset_y + cpu.get_height()) > 0 and (cpu_mouse_y + cpu_offset_y + cpu.get_height()) <= height):
+					cpu_pos[1] = cpu_mouse_y + cpu_offset_y
 
 	screen.fill(background_color)
+	# Add the animated widgets
 	animated_sprites.draw(screen)
 	animated_sprites.update()
+	# Draw the non-animated widgets
+	screen.blit(power, power_pos)
+	screen.blit(memory, memory_pos)
+	screen.blit(cpu, cpu_pos)
 	pygame.display.flip()
 	Clock.tick(MAX_FRAME_RATE)
---
+++ gamePy.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python
+import pyglet
+from pyglet.window import key
+
+class sprites(pyglet.sprite.Sprite):
+	def __init__(self, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+
+	def update(self, dx, dy):
+		self.x += dx
+		self.y += dy
+
+	def check_bounds(self, width, height):
+		min_x = -self.width / 2
+		min_y = -self.height / 2
+		max_x = width + self.width / 2
+		max_y = height + self.height / 2
+		if self.x < min_x:
+			self.x = max_x
+		elif self.x > max_x:
+			self.x = min_x
+		if self.y < min_y:
+			self.y = max_y
+		elif self.y > max_y:
+			self.y = min_y
+
+class game(pyglet.window.Window):
+	bin = pyglet.image.atlas.TextureBin()
+	def __init__(self):
+		super(game, self).__init__()
+		# Make the background white
+		pyglet.gl.glClearColor(1,1,1,1)
+
+		self.set_visible(False)
+
+		self.set_fullscreen(True)
+		self.set_vsync(True)
+		self.set_location(0, 0)
+
+		self.set_visible()
+
+		self.default_cursor = self.get_system_mouse_cursor(self.CURSOR_DEFAULT)
+		self.crosshair_cursor = self.get_system_mouse_cursor(self.CURSOR_CROSSHAIR)
+
+		self.main_batch = pyglet.graphics.Batch()
+		self.batches = [pyglet.graphics.Batch() for i in range(5)]
+		self.background = pyglet.graphics.OrderedGroup(0)
+		self.foreground = pyglet.graphics.OrderedGroup(1)
+
+		self.step = 0
+
+		self.step1 = sprites(pyglet.image.load('Images/Step1.png'), batch=self.batches[0], group=self.background, x = 0, y = 0)
+		self.step2 = sprites(pyglet.image.load('Images/step2.png'), batch=self.batches[1], group=self.background, x = 0, y = 0)
+		self.step3 = sprites(pyglet.image.load('Images/step3.png'), batch=self.batches[2], group=self.background, x = 0, y = 0)
+		self.step4 = sprites(pyglet.image.load('Images/step4.png'), batch=self.batches[3], group=self.background, x = 0, y = 0)
+		self.step5 = sprites(pyglet.image.load('Images/step5.png'), batch=self.batches[4], group=self.background, x = 0, y = 0)
+
+		self.arrow_clock_label = sprites(pyglet.image.load('Images/arrow.png'), batch=self.batches[0], group=self.foreground, x = 835, y = (self.height//3) + 50)
+		self.arrow_power_label = sprites(pyglet.image.load('Images/arrow.png'), batch=self.batches[1], group=self.foreground, x = 450, y = (self.height//3))
+		self.arrow_cpu_label = sprites(pyglet.image.load('Images/arrow.png'), batch=self.batches[2], group=self.foreground, x = 1680, y = (self.height//3) + 50)
+		self.arrow_memory_label = sprites(pyglet.image.load('Images/arrow.png'), batch=self.batches[3], group=self.foreground, x = 2340, y = (self.height//3) + 50)
+
+		self.next = sprites(pyglet.image.load('Images/next.png'), batch=self.main_batch, group=self.foreground, x = self.width - 400, y = 0)
+		self.next.scale_y = 1.2
+		self.back = sprites(pyglet.image.load('Images/back.png'), batch=self.main_batch, group=self.foreground, x = 0, y = 0)
+
+		# Labels
+		self.clock_label_1 = pyglet.text.Label(text="Hello, I'm a clock! I act as the 'heart' of your computer by pulsing high and low signals", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3), color=(0, 0, 0, 255), batch=self.batches[0], group=self.foreground)
+		self.clock_label_2 = pyglet.text.Label(text="to the rest of my system like a heart beats in your body. Each time I pulse, I output a high pulse", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-50, color=(0, 0, 0, 255), batch=self.batches[0], group=self.foreground)
+		self.clock_label_3 = pyglet.text.Label(text="that tells the computer to perform one instruction. The more times I pulse per second, the faster", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-100, color=(0, 0, 0, 255), batch=self.batches[0], group=self.foreground)
+
+		self.power_label_1 = pyglet.text.Label(text="Watts up! I'm food that goes into the belly of the computer! The CPU, Clock, Memory, and", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3), color=(0, 0, 0, 255), batch=self.batches[1], group=self.foreground)
+		self.power_label_2 = pyglet.text.Label(text="the Console all rely on me to keep them working in tip top shape! Just like you need food", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-50, color=(0, 0, 0, 255), batch=self.batches[1], group=self.foreground)
+		self.power_label_3 = pyglet.text.Label(text="to fill your stomach, computer's need electricity to live!", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-100, color=(0, 0, 0, 255), batch=self.batches[1], group=self.foreground)
+
+		self.cpu_label_1 = pyglet.text.Label(text="Greetings! I am the CPU of your computer, which stands for Central Processing Unit.", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3), color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_2 = pyglet.text.Label(text="Think of me as the brain of your computer, figuring out what the computer should do", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-50, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_3 = pyglet.text.Label(text="every time I get a clock pulse. In order to figure out what to do, I read a 'program',", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-100, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_4 = pyglet.text.Label(text="which is kind of like a blueprint of how to do something like display an Instagram page", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-150, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_5 = pyglet.text.Label(text="or do your taxes. But how do I run each instruction? Every instruction in your program", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-200, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_6 = pyglet.text.Label(text="includes a piece of data and an address. The address tells the CPU where the instruction", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-250, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_7 = pyglet.text.Label(text="is stored so that the CPU can find it! It's like knowing the address of your friend's", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-300, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_8 = pyglet.text.Label(text="house so you can find it. The data is the actual action to perform, like adding two numbers.", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-350, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+		self.cpu_label_9 = pyglet.text.Label(text="So if I know where my friends house is located, I can get directions from where I am to get there!", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-400, color=(0, 0, 0, 255), batch=self.batches[2], group=self.foreground)
+
+		self.memory_label_1 = pyglet.text.Label(text="Hi there! I'm your computer's memory. Think of me as the library of your computer that holds", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3), color=(0, 0, 0, 255), batch=self.batches[3], group=self.foreground)
+		self.memory_label_2 = pyglet.text.Label(text="your program and all the data that it needs to work. Remember how we talked about data and", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-50, color=(0, 0, 0, 255), batch=self.batches[3], group=self.foreground)
+		self.memory_label_3 = pyglet.text.Label(text="addresses? I hold both of these pieces to create a program that I send to the CPU. Think of", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-100, color=(0, 0, 0, 255), batch=self.batches[3], group=self.foreground)
+		self.memory_label_4 = pyglet.text.Label(text="a program like a very tall shelf with cubbies for each address that holds a piece of data.", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-150, color=(0, 0, 0, 255), batch=self.batches[3], group=self.foreground)
+		self.memory_label_5 = pyglet.text.Label(text="Every time the CPU needs some data, it just goes to the cubby or address where that data is,", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-200, color=(0, 0, 0, 255), batch=self.batches[3], group=self.foreground)
+		self.memory_label_6 = pyglet.text.Label(text="grabs it, and then does something with it!", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-250, color=(0, 0, 0, 255), batch=self.batches[3], group=self.foreground)
+
+		self.build_label_1 = pyglet.text.Label(text="Okay, now that we know about each of the pieces, how do we put them together? In the box to the", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3) - 100, color=(0, 0, 0, 255), batch=self.batches[4], group=self.foreground)
+		self.build_label_2 = pyglet.text.Label(text="left, you can click and drag the blocks to create connections. To start, let's plug in the power.", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-150, color=(0, 0, 0, 255), batch=self.batches[4], group=self.foreground)
+		self.build_label_3 = pyglet.text.Label(text="Take the cable and drag it into the outlet and the power input of the clock. Good job! Now you", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-200, color=(0, 0, 0, 255), batch=self.batches[4], group=self.foreground)
+		self.build_label_4 = pyglet.text.Label(text="need to connect the clock output to the CPU and the memory. Click and drag from the clock output", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-250, color=(0, 0, 0, 255), batch=self.batches[4], group=self.foreground)
+		self.build_label_5 = pyglet.text.Label(text="to the clock inputs of both of these blocks. Now that you're done with that, click and drag from", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-300, color=(0, 0, 0, 255), batch=self.batches[4], group=self.foreground)
+		self.build_label_6 = pyglet.text.Label(text="each of the data and address lines of the CPU to the memory. Make sure to match the numbers for each of the lines!", font_name="Comic Sans", font_size=30, x=self.width//2, anchor_x='center', y=(self.height//3)-350, color=(0, 0, 0, 255), batch=self.batches[4], group=self.foreground)
+
+		# For animating the heart
+		self.hearts = [pyglet.image.load(f'Images/heart{i}.png') for i in range(1, 5)]
+		self.hearts = [self.hearts[0], self.hearts[1], self.hearts[2], self.hearts[1], self.hearts[2], self.hearts[3], self.hearts[2], self.hearts[1], self.hearts[0]]
+		self.heart_animation = sprites(pyglet.image.Animation.from_image_sequence(self.hearts, duration=0.1, loop=True), batch=self.main_batch, group=self.foreground, x=(self.width/4.6), y=(self.height/1.9))
+		self.heart_animation.scale = 0.5
+
+	def on_draw(self):
+		self.render()
+
+	def render(self):
+		## == Clear the frame
+		self.clear()
+
+		self.batches[self.step].draw()
+
+		self.main_batch.draw()
+
+		## == And flip the current buffer to become the active viewed buffer.
+		self.flip()
+
+	def on_key_press(self, symbol, modifiers):
+		if symbol == key.ESCAPE:
+			print('The escape key was pressed.')
+			self.close()
+
+	def next_step(self):
+		self.step+= 1
+		if self.step == 5:
+			self.step = 0
+
+	def back_step(self):
+		self.step-= 1
+		if self.step == -1:
+			self.step = 0
+
+	# Handles the X button
+	def on_close(self):
+		print('The window was closed.')
+		self.set_visible(False)
+		self.close()
+
+	# Handles the mouse button presses
+	def on_mouse_press(self, x, y, button, modifiers):
+		if ((self.next.height > y) and ((self.width - self.next.x) < x) and (self.width - self.next.x + self.next.width < x)):
+			self.next_step()
+		elif ((self.back.height > y) and (self.back.x + self.back.width > x)):
+			self.back_step()
+		else:
+			print(f"Pressed ({x}, {y}). Next: ({self.next.x}, {self.next.y}) height {self.next.height}, Back: ({self.back.x}, {self.back.y})")
+		pass
+
+	def on_mouse_release(self, x, y, button, modifiers):
+		pass
+
+	def on_mouse_motion(self, x, y, dx, dy):
+		pass
+
+	# Required to drag items across the screen
+	def on_mouse_drag(self, x, y, dx, dy, button, modifiers):
+		# Checks if the mouse is over the heart animation while it was dragged, works
+		# if (((self.height - self.heart_animation.x - self.heart_animation.height) > x) and (self.heart_animation.x < x) and ((self.width - self.heart_animation.y - self.heart_animation.width) > y) and (self.heart_animation.y < y)):
+		# 	self.heart_animation.update(dx, dy)
+		# 	self.heart_animation.check_bounds(self.width, self.height)
+		pass
+
+	def on_mouse_enter(self, x, y):
+		pass
+
+	def on_mouse_leave(self, x, y):
+		pass
+
+	def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
+		pass
+
+if __name__ == '__main__':
+	window = game()
+	pyglet.app.run()
